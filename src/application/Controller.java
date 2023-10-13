36c84502f06ff3a71a8a9ab6f7b1e5a59ac7e318
OuBnOu7 <omar7benouahi@gmail.com>
2023-10-13 17:10:27 +0200

    first commit2

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -14,25 +14,7 @@ public class Controller {
 
     public void handleButtonClick(String value) {
         if (value.matches("[0-9]")) {
-            view.setCurrentInput(view.getCurrentInput() + value);
-        } else if (value.equals("C")) {
-            view.setCurrentInput("");
-            view.setOperator("");
-            view.clearDisplay();
-        } else if (value.equals("=")) {
-            if (!view.getCurrentInput().isEmpty() && !view.getOperator().isEmpty()) {
-                double num2 = Double.parseDouble(view.getCurrentInput());
-                double result = model.calculate(view.getFirstOperand(), num2, view.getOperator());
-                view.setResult(result);
-                view.setCurrentInput("");
-                view.setOperator("");
-            }
-        } else {
-            if (!view.getCurrentInput().isEmpty()) {
-                view.setOperator(value);
-                view.setFirstOperand(Double.parseDouble(view.getCurrentInput()));
-                view.setCurrentInput("");
-            }
-        }
+
     }
-}
+    }
+    }
